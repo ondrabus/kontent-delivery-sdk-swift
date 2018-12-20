0f12056dfe6da333b6d8f4323bb20663549d3abe
Martin Makarsky <martinmakarsky@gmail.com>
2018-12-20 09:41:54 +0100

    Remove tracker endpoint related code.

--- KenticoCloud/Classes/Shared/CloudConstants.swift
+++ KenticoCloud/Classes/Shared/CloudConstants.swift
@@ -9,7 +9,6 @@
 struct CloudConstants {
     static let liveDeliverEndpoint = "https://deliver.kenticocloud.com"
     static let previewDeliverEndpoint = "https://preview-deliver.kenticocloud.com"
-    static let trackerEndpoint = "https://engage-ket.kenticocloud.com/api/v1"
 }
 
 
